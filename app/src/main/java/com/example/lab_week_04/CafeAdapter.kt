b50212b239e09969921cd9aca03c8fa3aa698e01
sshivahh <russelshivah16@gmail.com>
2024-09-23 23:57:47 +0700

    hadu error

--- app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
+++ app/src/main/java/com/example/lab_week_04/CafeAdapter.kt
@@ -1,9 +1,14 @@
 package com.example.lab_week_04
 
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
 val TABS_FIXED = listOf(
-R.string.starbucks _ title,
-R.string.janjijiwa _ title,
-R.string.kopikenangan _ title,
+R.string.starbucks_title,
+R.string.janjijiwa_title,
+R.string.kopikenangan_title,
 )
 
 class CafeAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
